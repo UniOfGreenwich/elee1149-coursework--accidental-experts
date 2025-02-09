756339a26d5ae78b105f28853d80411f7a00346e
Patel, Nishal (N.N.) <npate170@ford.com>
2025-02-09 22:42:25 +0000

    Formatting Files and creating styling for home page goals component

--- Frontend/accidental-experts-frontend/src/Components/HomePageGoals/HomePageGoals.tsx
+++ Frontend/accidental-experts-frontend/src/Components/HomePageGoals/HomePageGoals.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import "./HomePageGoals.scss";
+import './HomePageGoals.scss';
 
 interface HomePageGoalsProps {
     heading: string;
@@ -13,12 +13,16 @@ class HomePageGoals extends Component<HomePageGoalsProps> {
         const { heading, text, imageUrl, backGroundColor } = this.props;
         return (
             <div className={`homePageGoalsContainer ${backGroundColor}`}>
-                <div className={"homePageGoalsHeadingWrapper"}>
+                <div className={'homePageGoalsHeadingWrapper'}>
                     <h2 className="homePageGoalsHeading">{heading}</h2>
                     <p className="homePageGoalsDescription">{text}</p>
                 </div>
                 <div>
-                    <img className="homePageGoalsImage" src={imageUrl} alt="Home Page Goal" />
+                    <img
+                        className="homePageGoalsImage"
+                        src={imageUrl}
+                        alt="Home Page Goal"
+                    />
                 </div>
             </div>
         );
--- Frontend/accidental-experts-frontend/src/Pages/home.tsx
+++ Frontend/accidental-experts-frontend/src/Pages/home.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import HomePageGoals from "../Components/HomePageGoals/HomePageGoals.tsx";
+import HomePageGoals from '../Components/HomePageGoals/HomePageGoals.tsx';
 
 export default function HomePage() {
     return (
         <div>
-            <HomePageGoals heading="Hello World" text="Hello World" imageUrl="https://via.placeholder.com/150" />
-            <HomePageGoals heading="Hello World" text="Hello World" imageUrl="https://via.placeholder.com/150" backGroundColor={"jaguarGreen"} />
-            <HomePageGoals heading="Hello World" text="Hello World" imageUrl="https://via.placeholder.com/150" />
+            <HomePageGoals
+                heading="Hello World"
+                text="Hello World"
+                imageUrl="https://via.placeholder.com/150"
+            />
+            <HomePageGoals
+                heading="Hello World"
+                text="Hello World"
+                imageUrl="https://via.placeholder.com/150"
+                backGroundColor={'jaguarGreen'}
+            />
+            <HomePageGoals
+                heading="Hello World"
+                text="Hello World"
+                imageUrl="https://via.placeholder.com/150"
+            />
         </div>
     );
 }

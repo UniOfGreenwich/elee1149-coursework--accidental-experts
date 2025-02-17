d71db79177e778f6fe5094ae252c2943cf7ff975
Patel, Nishal (N.N.) <npate170@ford.com>
2025-02-17 15:52:38 +0000

    Creating div structure and class name for sign in form

--- Frontend/accidental-experts-frontend/src/Components/LoginComponent/LoginComponent.tsx
+++ Frontend/accidental-experts-frontend/src/Components/LoginComponent/LoginComponent.tsx
@@ -21,33 +21,34 @@ const LoginComponent: React.FC = () => {
     return (
         <div>
             <h2>Sign In</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input placeholder={"Email"} type="email" {...register('email', {required: true})} />
-                {errors.email && <span>Email is required</span>}
-                <input
-                    placeholder={"Password"}
-                    type={showPassword ? "text" : "password"} {...register('password', {required: true})}
-                />
-                <button type="button" onClick={() => setShowPassword(!showPassword)}>
-                    {showPassword ? "Hide" : "Show"}
-                </button>
-                {errors.password &&
-                    <span>Password is required</span>}
-                <label>
-                    <input type="checkbox" {...register('rememberMe')}
-                           style={{display: 'inline-block', marginRight: '5px'}}/>
-                    Remember Me
-                </label>
-                <a href="/login-and-registration">Forgot Password?</a>
-                <input type="submit"/>
-
-            </form>
+            <div className="loginFormWrapper">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input placeholder={"Email"} type="email" {...register('email', {required: true})} />
+                    {errors.email && <span>Email is required</span>}
+                    <input
+                        placeholder={"Password"}
+                        type={showPassword ? "text" : "password"} {...register('password', {required: true})}
+                    />
+                    <button type="button" onClick={() => setShowPassword(!showPassword)}>
+                        {showPassword ? "Hide" : "Show"}
+                    </button>
+                    {errors.password &&
+                        <span>Password is required</span>}
+                    <label>
+                        <input type="checkbox" {...register('rememberMe')}
+                               style={{display: 'inline-block', marginRight: '5px'}}/>
+                        Remember Me
+                    </label>
+                    <a href="/login-and-registration">Forgot Password?</a>
+                    <input type="submit"/>
+                </form>
+            </div>
             <div className="separator">
                 <div className="separator-line"/>
                 <span className="separator-text">or</span>
                 <div className="separator-line"/>
             </div>
-            <div>
+            <div className="googleSignInWrapper">
                 <button className="google-signin-button">Sign in with Google</button>
             </div>
         </div>

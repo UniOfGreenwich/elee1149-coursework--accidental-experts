3b32c7050c6fbb4a95ee5a1411df93851d52316c
jp1911k <jp1911k@gre.ac.uk>
2025-03-11 09:20:22 +0000

    Header fixes

--- Frontend/accidental-experts-frontend/src/Components/Header/Header.tsx
+++ Frontend/accidental-experts-frontend/src/Components/Header/Header.tsx
@@ -1,43 +1,42 @@
 import React from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
 import './Header.scss'; // Import the SCSS file
-import { useRoutes } from '../../RoutesContext.tsx';
+import {useRoutes} from "../../RoutesContext.tsx";
 import { Link } from 'react-router-dom';
 
 export default function Header() {
     const logoURL =
         'https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExaWZvYXg5empwNDhtY3I5ZG5xOTBjOWlqMWNwN2FwaWw1azVqdWVuZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/VQXdNn4MMLmNtkhPCV/giphy.gif';
-    const { homePath, jobSearchPath, loginAndRegistrationPath } = useRoutes();
+    const { homePath, jobSearchPath } = useRoutes();
 
     return (
         <div className="headerWrapper">
-            <Row className="header">
+            <div className="header">
                 <div className="logoWrapper">
-                    <Col xs={3} className="logo-area">
+                    <div className="logo-area">
                         <img
                             src={logoURL}
                             alt="Accidentally Unemployed Logo"
                             className="logo"
                         />
-                        <Card.Text className="website-name">
+                        <div className="website-name">
                             Accidentally
                             <br />
                             Unemployed
-                        </Card.Text>
-                    </Col>
+                        </div>
+                    </div>
                 </div>
                 <div className="linksWrapper">
-                    <Col xs={3} className="login-signup">
-                        <Link to={loginAndRegistrationPath}>Log in</Link>
-                        <Link to={loginAndRegistrationPath}>Sign up</Link>
-                    </Col>
-                    <Col xs={6} className="nav-links">
+                    <div className="login-signup">
+                        <a href="#">Log in</a>
+                        <a href="#">Sign up</a>
+                    </div>
+                    <div className="nav-links">
                         <Link to={homePath}>Home</Link>
                         <Link to={jobSearchPath}>Job Search</Link>
                         <a href="#">Dashboard</a>
-                    </Col>
+                    </div>
                 </div>
-            </Row>
+            </div>
         </div>
     );
 }

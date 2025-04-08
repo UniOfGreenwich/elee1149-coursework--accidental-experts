c9e8a35a3cafc25e145bf8dedee81f95f1c8ddee
HGATELY <hg5664w@gre.ac.uk>
2025-04-08 17:34:57 +0100

    add endpoint

--- Backend/src/main/java/org/accidental/experts/backend/controller/JobController.java
+++ Backend/src/main/java/org/accidental/experts/backend/controller/JobController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -64,5 +65,11 @@ public class JobController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/active")
+    public List<Job> getActiveJobs() {
+        Instant currentDate = Instant.now();
+        return jobRepository.findActiveJobs(currentDate);
     }
 }
